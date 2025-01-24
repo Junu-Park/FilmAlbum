a48230238f56998814209a561cdcf8454d384053
Park Junwoo <ays99111@naver.com>
2025-01-24 18:09:21 +0900

    [feat] UILabel 텍스트 컬러 일괄 적용을 위한 UILabel.appearance() 변경 코드 추가

--- FilmAlbum/App/AppDelegate.swift
+++ FilmAlbum/App/AppDelegate.swift
@@ -15,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         UIView.appearance().backgroundColor = UIColor.faBlack
+        UILabel.appearance().textColor = UIColor.faWhite
         
         sleep(2)
         return true
