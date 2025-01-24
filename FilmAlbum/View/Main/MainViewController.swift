64404ec61ebd0425039b680bae25128cf1f8b656
Park Junwoo <ays99111@naver.com>
2025-01-24 21:34:11 +0900

    [feat] entry point 분기처리를 위한 MainVC 생성

---
+++ FilmAlbum/View/Main/MainViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MainViewController.swift
+//  FilmAlbum
+//
+//  Created by 박준우 on 1/24/25.
+//
+
+import UIKit
+
+final class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
