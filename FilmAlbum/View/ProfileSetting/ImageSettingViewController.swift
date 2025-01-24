a6df36581689bdba19ca1c28ee0992ec6182cbff
Park Junwoo <ays99111@naver.com>
2025-01-24 21:24:46 +0900

    [feat] ImageSettingVC과 NicknameSettingVC 생성

---
+++ FilmAlbum/View/ProfileSetting/ImageSettingViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ImageSettingViewController.swift
+//  FilmAlbum
+//
+//  Created by 박준우 on 1/24/25.
+//
+
+import UIKit
+
+class ImageSettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ FilmAlbum/View/ProfileSetting/NicknameSettingViewController.swift
@@ -0,0 +1,15 @@
+//
+//  NicknameSettingViewController.swift
+//  FilmAlbum
+//
+//  Created by 박준우 on 1/24/25.
+//
+
+import UIKit
+
+class NicknameSettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
