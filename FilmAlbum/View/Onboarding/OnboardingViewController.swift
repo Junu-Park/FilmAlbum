199628e4a477519c7289e60ef7176b84cba9806a
Park Junwoo <ays99111@naver.com>
2025-01-24 18:04:27 +0900

    [feat] OnboardingVC 생성

---
+++ FilmAlbum/View/Onboarding/OnboardingViewController.swift
@@ -0,0 +1,15 @@
+//
+//  OnboardingViewController.swift
+//  FilmAlbum
+//
+//  Created by 박준우 on 1/24/25.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
