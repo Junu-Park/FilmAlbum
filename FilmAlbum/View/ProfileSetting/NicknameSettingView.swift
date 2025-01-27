ba63b1e8b6540e470acf1c71c3ed167f5b046562
Park Junwoo <ays99111@naver.com>
2025-01-27 22:12:01 +0900

    [feat] NicknameSettingView completeButton 구현

--- FilmAlbum/View/ProfileSetting/NicknameSettingView.swift
+++ FilmAlbum/View/ProfileSetting/NicknameSettingView.swift
@@ -15,6 +15,14 @@ final class NicknameSettingView: UIView {
     
     private let nicknameTextFieldView: NicknameTextFieldView = NicknameTextFieldView()
     
+    private lazy var completeButton: RadiusBorderButton = {
+        let btn = RadiusBorderButton(title: "완료", radius: 25)
+        
+        btn.isEnabled = self.nicknameState == NicknameTextFieldState.ok
+        
+        return btn
+    }()
+    
     private var profileImageType: ProfileImage!
     
     private var nicknameState: NicknameTextFieldState! {
@@ -30,9 +38,11 @@ final class NicknameSettingView: UIView {
         self.profileImageType = profileImageType
         self.nicknameState = self.nicknameTextFieldView.nicknameTextField.text.checkNicknameValidation()
         self.nicknameTextFieldView.nicknameTextField.delegate = self
+        self.completeButton.addTarget(self, action: #selector(completeButtonTapped), for: .touchUpInside)
         
         self.addSubview(self.profileImageView)
         self.addSubview(self.nicknameTextFieldView)
+        self.addSubview(self.completeButton)
         
         self.profileImageView.snp.makeConstraints { make in
             make.top.equalTo(self.safeAreaLayoutGuide).offset(16)
@@ -43,6 +53,17 @@ final class NicknameSettingView: UIView {
             make.top.equalTo(self.profileImageView.snp.bottom).offset(32)
             make.horizontalEdges.equalToSuperview().inset(16)
         }
+        self.completeButton.snp.makeConstraints { make in
+            make.top.equalTo(self.nicknameTextFieldView.textFieldStateLabel.snp.bottom).offset(24)
+            make.horizontalEdges.equalToSuperview().inset(16)
+            make.height.equalTo(50)
+        }
+    }
+    
+    @objc private func completeButtonTapped() {
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene, let window = windowScene.windows.first else { return }
+        window.rootViewController = UINavigationController(rootViewController: MainViewController())
+        window.makeKeyAndVisible()
     }
     
     @available(*, unavailable)
