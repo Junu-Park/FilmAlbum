37e2a897db0d6c2274e36df84c9a4d6934dcaaee
Park Junwoo <ays99111@naver.com>
2025-01-28 01:07:36 +0900

    [fix] ProfileImage 미선택에서 선택으로 바뀔 경우 alpha 값 안 바뀌던 오류 수정

--- FilmAlbum/Extension/Extension+UIImageView.swift
+++ FilmAlbum/Extension/Extension+UIImageView.swift
@@ -9,6 +9,7 @@ import UIKit
 
 extension UIImageView {
     func selectedProfileImage() {
+        self.alpha = 1
         self.layer.borderColor = UIColor.accent.cgColor
         self.layer.borderWidth = 3
     }
