c815280ee0577e8323463fa0f116d22329ba5764
Park Junwoo <ays99111@naver.com>
2025-01-27 18:36:08 +0900

    [feat] NicknameSettingView에 ProfileImageView와 NicknameTextFieldView 적용

--- FilmAlbum/View/ProfileSetting/NicknameSettingView.swift
+++ FilmAlbum/View/ProfileSetting/NicknameSettingView.swift
@@ -11,9 +11,29 @@ import SnapKit
 
 final class NicknameSettingView: UIView {
 
+    private lazy var profileImageView: ProfileImageView = ProfileImageView(profileImageType: self.profileImageType, isSelected: true, showCamera: true, canTap: true)
+    
+    private let nicknameTextFieldView: NicknameTextFieldView = NicknameTextFieldView()
+    
+    private var profileImageType: ProfileImage!
+    
     init(profileImageType: ProfileImage) {
         super.init(frame: .zero)
         
+        self.profileImageType = profileImageType
+        
+        self.addSubview(self.profileImageView)
+        self.addSubview(self.nicknameTextFieldView)
+        
+        self.profileImageView.snp.makeConstraints { make in
+            make.top.equalTo(self.safeAreaLayoutGuide).offset(16)
+            make.size.equalTo(self.snp.width).multipliedBy(0.3)
+            make.centerX.equalToSuperview()
+        }
+        self.nicknameTextFieldView.snp.makeConstraints { make in
+            make.top.equalTo(self.profileImageView.snp.bottom).offset(32)
+            make.horizontalEdges.equalToSuperview().inset(16)
+        }
     }
     
     @available(*, unavailable)
