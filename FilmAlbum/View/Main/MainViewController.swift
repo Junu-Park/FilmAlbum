3d768b6ff7285b09947833d135efc6eeda31b99d
Park Junwoo <ays99111@naver.com>
2025-01-28 20:03:16 +0900

    [feat] MainVC ProfileBannerView 적용

--- FilmAlbum/View/Main/MainViewController.swift
+++ FilmAlbum/View/Main/MainViewController.swift
@@ -11,7 +11,7 @@ import SnapKit
 
 final class MainViewController: CustomBaseViewController {
 
-    private let userDataBannerView: UserDataBannerView = UserDataBannerView()
+    private let profileBannerView: ProfileBannerView = ProfileBannerView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
