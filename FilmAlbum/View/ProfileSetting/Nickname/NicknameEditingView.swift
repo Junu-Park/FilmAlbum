74d56a392749aee5517b2561ed4beaf09bb2d150
Park Junwoo <ays99111@naver.com>
2025-01-29 00:26:31 +0900

    [feat] NicknameEditing뷰 UI 구현

--- FilmAlbum/View/ProfileSetting/Nickname/NicknameEditingView.swift
+++ FilmAlbum/View/ProfileSetting/Nickname/NicknameEditingView.swift
@@ -7,11 +7,45 @@
 
 import UIKit
 
+import SnapKit
+
 class NicknameEditingView: UIView {
+    
+    lazy var profileImageView: ProfileImageView = ProfileImageView(profileImageType: self.profileImageType, isSelected: true, showCamera: true, canTap: true)
+    
+    let nicknameTextFieldView: NicknameTextFieldView = NicknameTextFieldView()
+    
+    var profileImageType: ProfileImage! {
+        didSet {
+            self.profileImageView.profileImageType = self.profileImageType
+        }
+    }
+    
+    var nicknameState: NicknameTextFieldState! {
+        didSet {
+            self.nicknameTextFieldView.textFieldStateLabel.text = self.nicknameState.rawValue
+        }
+    }
     
     init(profileImageType: ProfileImage) {
         super.init(frame: .zero)
         
+        self.profileImageType = profileImageType
+        self.nicknameTextFieldView.nicknameTextField.text = UserDataManager.getSetNickname()
+        self.nicknameState = self.nicknameTextFieldView.nicknameTextField.text.checkNicknameValidation()
+        
+        self.addSubview(self.profileImageView)
+        self.addSubview(self.nicknameTextFieldView)
+        
+        self.profileImageView.snp.makeConstraints { make in
+            make.top.equalTo(self.safeAreaLayoutGuide).offset(16)
+            make.size.equalTo(self.snp.width).multipliedBy(0.3)
+            make.centerX.equalToSuperview()
+        }
+        self.nicknameTextFieldView.snp.makeConstraints { make in
+            make.top.equalTo(self.profileImageView.snp.bottom).offset(32)
+            make.horizontalEdges.equalToSuperview().inset(16)
+        }
     }
     
     @available(*, unavailable)
