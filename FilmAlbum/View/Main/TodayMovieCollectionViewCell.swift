8e8d5c5d824b0d4039cac780dcaf08b74419d834
Park Junwoo <ays99111@naver.com>
2025-01-28 20:10:41 +0900

    [feat] MainVC에서 사용할 CollectionView 및 Cell 생성

---
+++ FilmAlbum/View/Main/TodayMovieCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  TodayMovieCollectionViewCell.swift
+//  FilmAlbum
+//
+//  Created by 박준우 on 1/28/25.
+//
+
+import UIKit
+
+class TodayMovieCollectionViewCell: UICollectionViewCell {
+    static let id: String = "TodayMovieCollectionViewCell"
+}
