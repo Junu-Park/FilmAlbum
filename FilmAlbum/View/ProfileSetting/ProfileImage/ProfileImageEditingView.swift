fe7990647bd18501f4223a36af2daeadbf606e8f
Park Junwoo <ays99111@naver.com>
2025-01-29 01:23:54 +0900

    [feat] ProfileImageEditingView 구현

--- FilmAlbum/View/ProfileSetting/ProfileImage/ProfileImageEditingView.swift
+++ FilmAlbum/View/ProfileSetting/ProfileImage/ProfileImageEditingView.swift
@@ -8,13 +8,88 @@
 import UIKit
 
 class ProfileImageEditingView: UIView {
-
-    init() {
+    
+    private lazy var selectedProfileImage: ProfileImageView = ProfileImageView(profileImageType: self.selectedProfileImageType, isSelected: true, showCamera: true, canTap: false)
+    
+    private lazy var profileImageCollectionView: ProfileImageCollectionView = {
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 24, bottom: 0, right: 24)
+        layout.minimumLineSpacing = 16
+        let sideSpacing: CGFloat = 24
+        let itemSpacing: CGFloat = 16
+        let width: Int = Int(UIScreen.main.bounds.width - (sideSpacing * 2) - (itemSpacing * 3)) / 4
+        layout.itemSize = CGSize(width: width, height: width)
+        
+        let cv: ProfileImageCollectionView = ProfileImageCollectionView(layout: layout)
+        return cv
+    }()
+    
+    var selectedProfileImageType: ProfileImage! {
+        didSet {
+            self.selectedProfileImage.profileImageType = self.selectedProfileImageType
+            self.profileImageCollectionView.reloadData()
+            closure?(self.selectedProfileImageType)
+        }
+    }
+    
+    private let profileImageList: [ProfileImage] = ProfileImage.allCases
+    
+    var closure: ((ProfileImage) -> ())?
+    
+    init(selectedProfileImageType: ProfileImage) {
         super.init(frame: .zero)
+        self.configureCollectionViewConnection()
+        self.selectedProfileImageType = selectedProfileImageType
+        
+        self.addSubview(selectedProfileImage)
+        self.addSubview(profileImageCollectionView)
+        
+        self.selectedProfileImage.snp.makeConstraints { make in
+            make.top.equalTo(self.safeAreaLayoutGuide).offset(16)
+            make.size.equalTo(self.snp.width).multipliedBy(0.3)
+            make.centerX.equalToSuperview()
+        }
+        self.profileImageCollectionView.snp.makeConstraints { make in
+            make.top.equalTo(self.selectedProfileImage.snp.bottom).offset(64)
+            make.horizontalEdges.bottom.equalToSuperview()
+        }
     }
     
     @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension ProfileImageEditingView: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func configureCollectionViewConnection() {
+        self.profileImageCollectionView.delegate = self
+        self.profileImageCollectionView.dataSource = self
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return profileImageList.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ProfileImageCollectionViewCell.id, for: indexPath) as? ProfileImageCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        cell.profileImageView.profileImageType = self.profileImageList[indexPath.item]
+        
+        if cell.profileImageView.profileImageType == self.selectedProfileImageType {
+            cell.profileImageView.isSelected = true
+        } else {
+            cell.profileImageView.isSelected = false
+        }
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if self.selectedProfileImageType != self.profileImageList[indexPath.item] {
+            self.selectedProfileImageType = self.profileImageList[indexPath.item]
+        }
     }
 }
--- FilmAlbum/View/ProfileSetting/ProfileImage/ProfileImageViewController.swift
+++ FilmAlbum/View/ProfileSetting/ProfileImage/ProfileImageViewController.swift
@@ -11,15 +11,21 @@ final class ProfileImageViewController: CustomBaseViewController {
 
     lazy var settingView: ProfileImageSettingView = ProfileImageSettingView(selectedProfileImageType: self.selectedProfileImageType)
     
-    private let editingView: ProfileImageEditingView = ProfileImageEditingView()
+    lazy var editingView: ProfileImageEditingView = ProfileImageEditingView(selectedProfileImageType: self.selectedProfileImageType)
     
     private var selectedProfileImageType: ProfileImage = ProfileImage.profile1 {
         didSet {
-            self.settingView.selectedProfileImageType = self.selectedProfileImageType
+            if self.viewType == .imageSetting {
+                self.settingView.selectedProfileImageType = self.selectedProfileImageType
+            } else if self.viewType == .imageEditing {
+                self.editingView.selectedProfileImageType = self.selectedProfileImageType
+            } else {
+                print(#function, "viewType error")
+            }
         }
     }
     
-    init(viewType: ViewType , selectedProfileImageType: ProfileImage!) {
+    init(viewType: ViewType , selectedProfileImageType: ProfileImage) {
         super.init(viewType: viewType)
         self.selectedProfileImageType = selectedProfileImageType
         self.settingView.selectedProfileImageType = self.selectedProfileImageType
