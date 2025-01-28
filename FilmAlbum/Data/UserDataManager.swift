8fee09070a2754c0999e0aeb4cdd01d77799b586
Park Junwoo <ays99111@naver.com>
2025-01-28 16:18:30 +0900

    [feat] UserDefaults를 이용한 UserData 관리 클래스 추가

---
+++ FilmAlbum/Data/UserDataManager.swift
@@ -0,0 +1,42 @@
+//
+//  UserDataManager.swift
+//  FilmAlbum
+//
+//  Created by 박준우 on 1/28/25.
+//
+
+import Foundation
+
+enum UserDefaultsKey: String, CaseIterable {
+    case nickname = "Nickname"
+    case profileImage = "ProfileImage"
+}
+
+class UserDataManager {
+    
+    private init() {}
+    
+    @discardableResult static func getSetNickname(newNickname nickname: String? = nil) -> String {
+        if let nickname {
+            UserDefaults.standard.set(nickname, forKey: UserDefaultsKey.nickname.rawValue)
+            return nickname
+        } else {
+            return UserDefaults.standard.string(forKey: UserDefaultsKey.nickname.rawValue) ?? ""
+        }
+    }
+    
+    @discardableResult static func getSetProfileImage(newProfileImageType profileImageType: ProfileImage? = nil) -> ProfileImage {
+        if let profileImageType {
+            UserDefaults.standard.set(profileImageType.rawValue, forKey: UserDefaultsKey.profileImage.rawValue)
+            return profileImageType
+        } else {
+            return ProfileImage(rawValue: UserDefaults.standard.integer(forKey: UserDefaultsKey.profileImage.rawValue)) ?? ProfileImage.randomCase
+        }
+    }
+    
+    static func resetUserData() {
+        UserDefaultsKey.allCases.forEach { key in
+            UserDefaults.standard.removeObject(forKey: key.rawValue)
+        }
+    }
+}
