5a0eb775cba77e77de8d79c2f0eb0b7c4ad0b6ab
Park Junwoo <ays99111@naver.com>
2025-02-01 04:11:32 +0900

    [feat] Search뷰 장르 표시 기능 추가

--- FilmAlbum/View/Tab/Cinema/Search/SearchViewController.swift
+++ FilmAlbum/View/Tab/Cinema/Search/SearchViewController.swift
@@ -10,6 +10,72 @@ import UIKit
 import Kingfisher
 import SnapKit
 
+enum Genre: Int {
+    case action = 28
+    case animation = 16
+    case crime = 80
+    case drama = 18
+    case fantasy = 14
+    case horror = 27
+    case mystery = 9648
+    case sf = 878
+    case thriller = 53
+    case western = 37
+    case adventure = 12
+    case comedy = 35
+    case documentary = 99
+    case family = 10751
+    case history = 36
+    case music = 10402
+    case romance = 10749
+    case tvMovie = 10770
+    case war = 10752
+    
+    var title: String {
+        switch self {
+        case .action: 
+            return "액션"
+        case .animation: 
+            return "애니메이션"
+        case .crime: 
+            return "범죄"
+        case .drama: 
+            return "드라마"
+        case .fantasy: 
+            return "판타지"
+        case .horror: 
+            return "공포"
+        case .mystery: 
+            return "미스터리"
+        case .sf: 
+            return "SF"
+        case .thriller: 
+            return "스릴러"
+        case .western: 
+            return "서부"
+        case .adventure: 
+            return "모험"
+        case .comedy: 
+            return "코미디"
+        case .documentary: 
+            return "다큐멘터리"
+        case .family: 
+            return "가족"
+        case .history: 
+            return "역사"
+        case .music: 
+            return "음악"
+        case .romance: 
+            return "로맨스"
+        case .tvMovie: 
+            return "TV 영화"
+        case .war: 
+            return "전쟁"
+        }
+    }
+}
+
+
 final class SearchViewController: CustomBaseViewController {
 
     private let searchCollectionView: SearchCollectionView = SearchCollectionView(layout: UICollectionViewFlowLayout())
@@ -79,7 +145,7 @@ extension SearchViewController: UISearchBarDelegate {
         UserDataManager.getSetLikeMovieList(newLikeMovieIDList: list)
         NotificationCenter.default.post(name: NSNotification.Name("LikeButtonTapped"), object: nil, userInfo: ["isCinemaCollectionViewReload": true])
         UIView.performWithoutAnimation {
-            self.searchCollectionView.reloadItems(at: [IndexPath(row: sender.tag, section: 0)])
+            self.searchCollectionView.reloadItems(at: [IndexPath(item: sender.tag, section: 0)])
         }
     }
 }
@@ -98,17 +164,26 @@ extension SearchViewController: UICollectionViewDelegate, UICollectionViewDataSo
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: SearchCollectionViewCell.id, for: indexPath) as? SearchCollectionViewCell {
             
-            if UserDataManager.getSetLikeMovieList().contains(self.searchResponse.results[indexPath.row].id) {
+            if UserDataManager.getSetLikeMovieList().contains(self.searchResponse.results[indexPath.item].id) {
                 cell.likeButton.setImage(UIImage.faHeartFill, for: .normal)
             } else {
                 cell.likeButton.setImage(UIImage.faHeart, for: .normal)
             }
-            cell.tag = indexPath.row
-            cell.titleLabel.text = self.searchResponse.results[indexPath.row].title
-            cell.dateLabel.text = self.searchResponse.results[indexPath.row].release_date.replaceLineWithPoint()
-            cell.posterImageView.kf.setImage(with: URL(string: TMDBAPI.image200Base + (self.searchResponse.results[indexPath.row].poster_path ?? "")))
+            cell.tag = indexPath.item
+            cell.titleLabel.text = self.searchResponse.results[indexPath.item].title
+            cell.dateLabel.text = self.searchResponse.results[indexPath.item].release_date.replaceLineWithPoint()
+            cell.posterImageView.kf.setImage(with: URL(string: TMDBAPI.image200Base + (self.searchResponse.results[indexPath.item].poster_path ?? "")))
+            
+            if let genre1 = self.searchResponse.results[indexPath.item].genre_ids.first {
+                cell.genre1Label.text = Genre.init(rawValue: genre1)?.title
+            }
+            if self.searchResponse.results[indexPath.item].genre_ids.count > 1 {
+                cell.genre2Label.text =
+                Genre.init(rawValue: self.searchResponse.results[indexPath.item].genre_ids[1])?.title
+            }
+            
             cell.likeButton.addTarget(self, action: #selector(self.likeButtonTapped), for: .touchUpInside)
-            cell.likeButton.tag = indexPath.row
+            cell.likeButton.tag = indexPath.item
             return cell
         } else {
             return UICollectionViewCell()
