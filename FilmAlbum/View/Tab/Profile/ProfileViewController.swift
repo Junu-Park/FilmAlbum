db52648f291de2f0cdfe2650921783d5d81435fd
Park Junwoo <ays99111@naver.com>
2025-01-31 14:21:09 +0900

    [feat] ProfileVC 및 UpcomingVC 생성

---
+++ FilmAlbum/View/Tab/Profile/ProfileViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ProfileViewController.swift
+//  FilmAlbum
+//
+//  Created by 박준우 on 1/31/25.
+//
+
+import UIKit
+
+final class ProfileViewController: CustomBaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
---
+++ FilmAlbum/View/Tab/Upcoming/UpcomingViewController.swift
@@ -0,0 +1,16 @@
+//
+//  UpcomingViewController.swift
+//  FilmAlbum
+//
+//  Created by 박준우 on 1/31/25.
+//
+
+import UIKit
+
+final class UpcomingViewController: CustomBaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
