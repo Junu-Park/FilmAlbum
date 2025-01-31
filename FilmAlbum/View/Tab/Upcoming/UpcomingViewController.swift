c9038483a53c3364474b153edf8ffcf953988c7b
Park Junwoo <ays99111@naver.com>
2025-01-31 14:33:05 +0900

    [feat] UpcomingVC 임시 뷰 추가

--- FilmAlbum/View/Tab/Upcoming/UpcomingViewController.swift
+++ FilmAlbum/View/Tab/Upcoming/UpcomingViewController.swift
@@ -7,10 +7,24 @@
 
 import UIKit
 
+import SnapKit
+
 final class UpcomingViewController: CustomBaseViewController {
 
+    private let tempLabel: UILabel = {
+        let lb: UILabel = UILabel()
+        lb.font = UIFont.fa16BoldFont
+        lb.text = "Coming Soon"
+        return lb
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.view.addSubview(self.tempLabel)
+        
+        self.tempLabel.snp.makeConstraints { make in
+            make.center.equalTo(self.view.safeAreaLayoutGuide)
+        }
     }
 }
