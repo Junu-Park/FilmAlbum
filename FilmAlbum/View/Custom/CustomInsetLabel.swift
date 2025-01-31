2fb0288e722873254daf1b8670dd6774e3246348
Park Junwoo <ays99111@naver.com>
2025-02-01 03:07:15 +0900

    [fix] Search API backdrop_path 데이터 nil인 경우 오류

---
+++ FilmAlbum/View/Custom/CustomInsetLabel.swift
@@ -0,0 +1,8 @@
+//
+//  CustomInsetLabel.swift
+//  FilmAlbum
+//
+//  Created by 박준우 on 1/31/25.
+//
+
+import Foundation
---
+++ FilmAlbum/View/Tab/Cinema/Search/SearchCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  SearchCollectionViewCell.swift
+//  FilmAlbum
+//
+//  Created by 박준우 on 1/31/25.
+//
+
+import UIKit
+
+class SearchCollectionViewCell: UICollectionViewCell {
+    
+}
