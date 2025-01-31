25a6f947f7c4fa7ddbd83be46acacbc819a49d1c
Park Junwoo <ays99111@naver.com>
2025-01-31 18:13:42 +0900

    [feat] Search뷰 SearchBar 구현

--- FilmAlbum/Extension/Extension+UIViewController.swift
+++ FilmAlbum/Extension/Extension+UIViewController.swift
@@ -22,4 +22,20 @@ extension UIViewController {
         
         return ac
     }
+    
+    func setNavigationItemSearchBar() {
+        let sc: UISearchController = UISearchController()
+        sc.hidesNavigationBarDuringPresentation = false
+        sc.automaticallyShowsCancelButton = false
+        sc.searchBar.searchTextField.tintColor = UIColor.faWhite
+        sc.searchBar.searchTextField.leftView?.tintColor = UIColor.faWhite
+        sc.searchBar.searchTextField.attributedPlaceholder = NSAttributedString(string: "검색어를 입력하세요.", attributes: [.foregroundColor: UIColor.faGray])
+        sc.searchBar.searchTextField.backgroundColor = .faDarkGray
+        
+        DispatchQueue.main.async {
+            sc.searchBar.searchTextField.textColor = UIColor.faWhite
+        }
+        
+        self.navigationItem.searchController = sc
+    }
 }
--- FilmAlbum/View/Tab/Cinema/Search/SearchViewController.swift
+++ FilmAlbum/View/Tab/Cinema/Search/SearchViewController.swift
@@ -11,6 +11,18 @@ final class SearchViewController: CustomBaseViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
+        self.setNavigationItemSearchBar()
+        self.navigationItem.searchController?.searchBar.delegate = self
+    }
+}
+extension SearchViewController: UISearchBarDelegate {
+    
+    func searchBarShouldEndEditing(_ searchBar: UISearchBar) -> Bool {
+        if let searchTerm = searchBar.text, !searchTerm.isEmpty {
+            return true
+        } else {
+            return false
+        }
     }
 }
