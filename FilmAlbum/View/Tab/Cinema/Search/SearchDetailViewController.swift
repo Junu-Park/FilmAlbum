3c893f6fd1d015c68bdeecba1de31dcd769f6b1c
Park Junwoo <ays99111@naver.com>
2025-02-01 15:22:29 +0900

    [feat] SearchDetail뷰 Synopsis 추가

--- FilmAlbum/View/Tab/Cinema/Search/SearchDetailViewController.swift
+++ FilmAlbum/View/Tab/Cinema/Search/SearchDetailViewController.swift
@@ -18,6 +18,37 @@ final class SearchDetailViewController: CustomBaseViewController {
         return sv
     }()
     private lazy var detailDataView: SearchDetailDataView = SearchDetailDataView(data: self.movieData)
+    private let synopsisTitle: UILabel = {
+        let lb: UILabel = UILabel()
+        lb.font = UIFont.fa14BoldFont
+        lb.text = "Synopsis"
+        return lb
+    }()
+    private lazy var synopsisButton: UIButton = {
+        let btn: UIButton = UIButton()
+        btn.setAttributedTitle(NSAttributedString(string: "More", attributes: [.foregroundColor: UIColor.faAccent, .font: UIFont.fa14BoldFont]), for: .normal)
+        btn.setAttributedTitle(NSAttributedString(string: "More", attributes: [.foregroundColor: UIColor.faLightGray, .font: UIFont.fa14BoldFont]), for: .highlighted)
+        btn.addTarget(self, action: #selector(self.synopsisButtonTapped), for: .touchUpInside)
+        return btn
+    }()
+    private let synopsisLabel: UILabel = {
+        let lb: UILabel = UILabel()
+        lb.font = UIFont.fa14Font
+        lb.numberOfLines = 3
+        return lb
+    }()
+    private let castLabel: UILabel = {
+        let lb: UILabel = UILabel()
+        lb.font = UIFont.fa14BoldFont
+        lb.text = "Cast"
+        return lb
+    }()
+    private let posterLabel: UILabel = {
+        let lb: UILabel = UILabel()
+        lb.font = UIFont.fa14BoldFont
+        lb.text = "Poster"
+        return lb
+    }()
     
     var movieData: SearchResult = SearchResult(id: 0, backdrop_path: "", title: "", overview: "", poster_path: "", genre_ids: [], release_date: "", vote_average: 0)
     var movieImage: ImageResponse = ImageResponse(id: 0, backdrops: [], posters: [])
@@ -34,6 +65,7 @@ final class SearchDetailViewController: CustomBaseViewController {
         NetworkManager.requestTMDB(type: .credit(movieID: self.movieData.id, params: CreditRequest())) { (response: CreditResponse) in
             self.movieCredit = response
         }
+        self.synopsisLabel.text = self.movieData.overview
     }
     
     override func viewDidLoad() {
@@ -42,6 +74,9 @@ final class SearchDetailViewController: CustomBaseViewController {
         self.view.addSubview(self.mainScrollView)
         self.mainScrollView.addSubview(self.backdropScrollView)
         self.mainScrollView.addSubview(self.detailDataView)
+        self.mainScrollView.addSubview(self.synopsisTitle)
+        self.mainScrollView.addSubview(self.synopsisButton)
+        self.mainScrollView.addSubview(self.synopsisLabel)
         
         self.mainScrollView.snp.makeConstraints { make in
             make.edges.equalTo(self.view.safeAreaLayoutGuide)
@@ -53,6 +88,18 @@ final class SearchDetailViewController: CustomBaseViewController {
         self.detailDataView.snp.makeConstraints { make in
             make.top.equalTo(self.backdropScrollView.snp.bottom).offset(16)
             make.centerX.equalToSuperview()
+        }
+        self.synopsisTitle.snp.makeConstraints { make in
+            make.top.equalTo(self.detailDataView.snp.bottom).offset(32)
+            make.leading.equalToSuperview().offset(16)
+        }
+        self.synopsisButton.snp.makeConstraints { make in
+            make.top.equalTo(self.detailDataView.snp.bottom).offset(32)
+            make.trailing.equalToSuperview().inset(16)
+        }
+        self.synopsisLabel.snp.makeConstraints { make in
+            make.top.equalTo(self.synopsisTitle.snp.bottom).offset(8)
+            make.horizontalEdges.equalToSuperview().inset(16)
         }
     }
     
@@ -76,5 +123,17 @@ final class SearchDetailViewController: CustomBaseViewController {
         self.navigationItem.rightBarButtonItem?.image = image
         
         NotificationCenter.default.post(name: NSNotification.Name("LikeButtonTapped"), object: nil, userInfo: ["isSearchDetail": true])
+    }
+    
+    @objc private func synopsisButtonTapped() {
+        if self.synopsisLabel.numberOfLines == 3 {
+            self.synopsisLabel.numberOfLines = 0
+            self.synopsisButton.setAttributedTitle(NSAttributedString(string: "Hide", attributes: [.foregroundColor: UIColor.faAccent, .font: UIFont.fa14BoldFont]), for: .normal)
+            self.synopsisButton.setAttributedTitle(NSAttributedString(string: "Hide", attributes: [.foregroundColor: UIColor.faLightGray, .font: UIFont.fa14BoldFont]), for: .highlighted)
+        } else {
+            self.synopsisLabel.numberOfLines = 3
+            self.synopsisButton.setAttributedTitle(NSAttributedString(string: "More", attributes: [.foregroundColor: UIColor.faAccent, .font: UIFont.fa14BoldFont]), for: .normal)
+            self.synopsisButton.setAttributedTitle(NSAttributedString(string: "More", attributes: [.foregroundColor: UIColor.faLightGray, .font: UIFont.fa14BoldFont]), for: .highlighted)
+        }
     }
 }
