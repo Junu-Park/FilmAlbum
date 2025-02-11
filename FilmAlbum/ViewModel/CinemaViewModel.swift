dc32f1ab60dd8318118951282629ef236a48c34f
Park Junwoo <ays99111@naver.com>
2025-02-11 18:03:25 +0900

    [feat] CinemaViewModel 생성

---
+++ FilmAlbum/ViewModel/CinemaViewModel.swift
@@ -0,0 +1,32 @@
+//
+//  CinemaViewModel.swift
+//  FilmAlbum
+//
+//  Created by 박준우 on 2/11/25.
+//
+
+import Foundation
+
+final class CinemaViewModel: ViewModelProtocol {
+    struct Input {
+        let viewInit: Observer<()> = Observer(value: ())
+    }
+    struct Output {
+        let trendData: Observer<Array<TrendingResult>> = Observer(value: [])
+    }
+    
+    var input: Input = Input()
+    var output: Output = Output()
+    
+    init() {
+        self.transform()
+    }
+    
+    func transform() {
+        self.input.viewInit.bindWithInit { _, nV in
+            NetworkManager.requestTMDB(type: .trending(params: TrendingRequest())) { [weak self] (response: TrendingResponse) in
+                self?.output.trendData.value = response.results
+            }
+        }
+    }
+}
