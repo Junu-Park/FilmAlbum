814038f785c3adcb67957928b4b82f660d7f461a
Park Junwoo <ays99111@naver.com>
2025-03-01 12:39:04 +0900

    [refactor] 📝 특정 로직에서만 사용되는 메서드 종속적으로 구성

--- FilmAlbum/Extension/Extension+UIViewController.swift
+++ FilmAlbum/Extension/Extension+UIViewController.swift
@@ -8,21 +8,6 @@
 import UIKit
 
 extension UIViewController {
-    func getResignAlert() -> UIAlertController {
-        let ac: UIAlertController = UIAlertController(title: "탈퇴하기", message: "탈퇴를 하면 데이터가 모두 초기화됩니다.\n탈퇴 하시겠습니까?", preferredStyle: .alert)
-        let confirm: UIAlertAction = UIAlertAction(title: "확인", style: .default) { _ in
-            UserDataManager.resetUserData()
-            guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene, let window = windowScene.windows.first else { return }
-            window.rootViewController = UINavigationController(rootViewController: OnboardingViewController(viewType: .onboarding))
-            window.makeKeyAndVisible()
-        }
-        let cancel: UIAlertAction = UIAlertAction(title: "취소", style: .cancel)
-        ac.addAction(confirm)
-        ac.addAction(cancel)
-        
-        return ac
-    }
-    
     func setNavigationItemSearchBar() {
         let sc: UISearchController = UISearchController()
         sc.hidesNavigationBarDuringPresentation = false
